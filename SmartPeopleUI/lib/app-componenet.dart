959c34228b280d813b78945865f2f418f7afed54
Alexandr <a.kabanov.pz@gmail.com>
2016-04-05 22:40:08 +0300

    Added some routing steps

--- SmartPeopleUI/lib/app-componenet.dart
+++ SmartPeopleUI/lib/app-componenet.dart
@@ -1,12 +1,26 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 
-import 'header/login-componenet.dart';
-import 'system-control-room/sign-up/sign-up-componenet.dart';
-import 'footer/footer-componenet.dart';
+import 'package:SmartPeopleUI/header/login-componenet.dart';
+import 'package:SmartPeopleUI/system-control-room/sign-up/sign-up-componenet.dart';
+import 'package:SmartPeopleUI/footer/footer-componenet.dart';
 
 @Component(
     selector: 'body',
-    directives: const [LoginComponent, SignUpComponent, FooterComponent],
+    directives: const [ROUTER_DIRECTIVES],
+    providers: const [ROUTER_PROVIDERS],
     styleUrls: const ['app.css'],
     templateUrl: 'app-view.html')
-class AppComponent {}
+@RouteConfig(const [
+   const Route(
+       path: '/login',
+       name: 'LoginOrSignUp',
+       component: LoginComponent,
+       useAsDefault: true),
+   const Route(
+       path: '/signup', name: 'signup', component: SignUpComponent),
+   const Route(path: '/footer', name: 'footer', component: FooterComponent)
+])
+class AppComponent {
+   String title = 'Smart People - is the one scientific social network';
+}
