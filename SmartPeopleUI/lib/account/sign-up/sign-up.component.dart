46a560846714b7274caf18de3e47c657294a1187
sanya-terror <a.kabanov.pz@gmail.com>
2016-05-09 00:06:30 +0300

    Added and adjusted Sign Up component

---
+++ SmartPeopleUI/lib/account/sign-up/sign-up.component.dart
@@ -0,0 +1,12 @@
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+@Component(
+    selector: 'sign-up',
+    directives: const [
+       ROUTER_DIRECTIVES
+    ],
+    styleUrls: const ['sign-up.component.css'],
+    templateUrl: 'sign-up.component.html')
+
+class SignUpComponent {}
