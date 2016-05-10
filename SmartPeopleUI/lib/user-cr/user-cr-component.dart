0bd55e9882ea8a6f9005b83b013679c3c715e201
ne4istb <anechytailov@gmail.com>
2016-05-10 20:43:57 +0300

    Removed user-cr component as it is tottally unusefull now

--- SmartPeopleUI/lib/user-cr/user-cr-component.dart
+++
@@ -1,9 +0,0 @@
-import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
-
-@Component(
-    selector: 'my-app',
-    directives: const [ROUTER_DIRECTIVES],
-    templateUrl: 'user-cr-view.html')
-
-class UserCrComponent {}
