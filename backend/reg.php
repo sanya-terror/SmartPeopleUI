68ff36aa38055223d41af9f113082ff7403a4e8c
Aleksander Kabanov <okabanov@twinfield.com>
2016-05-13 02:44:35 +0300

    [Phase-1] removed extra code

--- backend/reg.php
+++
@@ -1,7 +0,0 @@
-<?php
-	header("Content-Type: text/html; charset=utf-8");
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/library/class.registration.php');
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/backend/library/class.main.php');
-	$res = new register();
-	$res->mail();
-?>
