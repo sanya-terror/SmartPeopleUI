d961987c453064079f01d8f0a9ac78085ec79448
Aleksander Kabanov <okabanov@twinfield.com>
2016-05-13 02:42:01 +0300

    [SP-4] used validation directives in sing up component

--- SmartPeopleUI/lib/account/sign-up/sign-up.component.dart
+++ SmartPeopleUI/lib/account/sign-up/sign-up.component.dart
@@ -1,12 +1,24 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 
+import 'directives/index.dart';
+import 'package:SmartPeopleUI/shared/directives/index.dart';
+
 @Component(
     selector: 'sign-up',
     directives: const [
-       ROUTER_DIRECTIVES
+       ROUTER_DIRECTIVES,
+       EmailValidatorDirective,
+       PasswordValidatorDirective,
+       NameValidatorDirective
     ],
     styleUrls: const ['sign-up.component.css'],
     templateUrl: 'sign-up.component.html')
 
-class SignUpComponent {}
+class SignUpComponent {
+   String email;
+   String password1;
+   String password2;
+   String name;
+   String surname;
+}
