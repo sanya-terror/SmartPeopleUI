9a99dcb928b18c8d0ad1e4bd927b7a917b8dc348
Aleksander Kabanov <okabanov@twinfield.com>
2016-05-13 03:21:39 +0300

    [SP-49] used validator directives in cnange password component

--- SmartPeopleUI/lib/account/change-password/change-password.component.dart
+++ SmartPeopleUI/lib/account/change-password/change-password.component.dart
@@ -1,16 +1,23 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
+
 import 'package:angular2_material/src/components/button/button.dart';
 import 'package:SmartPeopleUI/shared/index.dart';
+
+import 'package:SmartPeopleUI/shared/directives/index.dart';
 
 @Component(
     selector: 'change-password',
     directives: const [
        ROUTER_DIRECTIVES,
+       PasswordValidatorDirective,
        MdAnchor,
        InfoComponent
     ],
     templateUrl: 'change-password.component.html',
     styleUrls: const['change-password.component.css'])
 
-class ChangePasswordComponent {}
+class ChangePasswordComponent {
+   String password1;
+   String password2;
+}
