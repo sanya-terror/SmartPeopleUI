5476c7d492d8cfb28b2d8dab36f10f8385f1590b
Aleksander Kabanov <okabanov@twinfield.com>
2016-05-13 03:09:32 +0300

    [SP-48] used validation directives in error auth component

--- SmartPeopleUI/lib/account/error-authorization/error-authorization.component.dart
+++ SmartPeopleUI/lib/account/error-authorization/error-authorization.component.dart
@@ -1,16 +1,24 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
+
 import 'package:angular2_material/src/components/button/button.dart';
 import 'package:SmartPeopleUI/shared/index.dart';
+
+import 'package:SmartPeopleUI/shared/directives/index.dart';
 
 @Component(
     selector: 'change-password',
     directives: const [
        ROUTER_DIRECTIVES,
+       EmailValidatorDirective,
+       PasswordValidatorDirective,
        MdAnchor,
        InfoComponent
     ],
     templateUrl: 'error-authorization.component.html',
     styleUrls: const['error-authorization.component.css'])
 
-class ErrorAuthorizationComponent {}
+class ErrorAuthorizationComponent {
+   String email;
+   String password;
+}
