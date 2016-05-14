da3179480b5fdc09127dc817ae6db9718bfcd36e
sanya-terror <a.kabanov.pz@gmail.com>
2016-05-14 22:27:36 +0300

    [SP-3] Moved login-form initialization to component logic

--- lib/shared/header/login/login.component.dart
+++ lib/shared/header/login/login.component.dart
@@ -1,17 +1,16 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/common.dart';
 import 'package:angular2/router.dart';
 
 import 'package:angular2_material/src/components/button/button.dart';
 import 'package:angular2_material/src/components/checkbox/checkbox.dart';
 
-import 'package:SmartPeopleUI/shared/directives/index.dart';
+import 'package:SmartPeopleUI/shared/validators/index.dart';
 
 @Component(
     selector: 'sp-login',
     directives: const [
        ROUTER_DIRECTIVES,
-       EmailValidatorDirective,
-       PasswordValidatorDirective,
        MdButton,
        MdCheckbox
     ],
@@ -19,7 +18,14 @@ import 'package:SmartPeopleUI/shared/directives/index.dart';
     encapsulation: ViewEncapsulation.Native,
     styleUrls: const ['login.component.css'])
 
-class LoginComponent {
-  String email;
-  String password;
+class LoginComponent{
+
+   ControlGroup form;
+
+   LoginComponent() {
+      this.form = new ControlGroup({
+         'email': new Control('', Validators.compose([EmailValidator.validate, Validators.required])),
+         'password': new Control('', Validators.compose([PasswordValidator.validate, Validators.required]))
+      });
+   }
 }
