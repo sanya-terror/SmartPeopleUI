cee52126b610cfb0097af627c7bdbed33d7f044f
Aleksander Kabanov <okabanov@twinfield.com>
2016-05-16 15:17:29 +0300

    Added validation notification in LoginComponent

--- lib/shared/header/login/login.component.dart
+++ lib/shared/header/login/login.component.dart
@@ -23,9 +23,13 @@ class LoginComponent{
    ControlGroup form;
 
    LoginComponent() {
+
       this.form = new ControlGroup({
          'email': new Control('', Validators.compose([EmailValidator.validate, Validators.required])),
          'password': new Control('', Validators.compose([PasswordValidator.validate, Validators.required]))
       });
+
+      print(this.form.controls['email'].valid);
+      print(this.form.controls['email'].value);
    }
 }
