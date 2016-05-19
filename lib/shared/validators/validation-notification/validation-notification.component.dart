48835f1e605b139d955708bdbc7f9325fa55bcd5
Andrii Nechytailov <anechytailov@gmail.com>
2016-05-19 12:58:36 +0300

    Removed redundant route directive

--- lib/shared/validators/validation-notification/validation-notification.component.dart
+++ lib/shared/validators/validation-notification/validation-notification.component.dart
@@ -1,9 +1,7 @@
 import 'package:angular2/core.dart';
-import 'package:angular2/router.dart';
 
 @Component(
   selector: 'sp-validation-notification',
-  directives: const [ROUTER_DIRECTIVES],
   templateUrl: 'validation-notification.component.html',
   styleUrls: const ['validation-notification.component.css'])
 
