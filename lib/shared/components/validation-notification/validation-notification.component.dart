bbd3ff59921e010d12a7a59be4b654a66cf792b2
Ne4istb <anechytailov@gmail.com>
2016-06-18 20:11:14 +0300

    [SP-76] Replaced elements in login component

--- lib/shared/components/validation-notification/validation-notification.component.dart
+++ lib/shared/components/validation-notification/validation-notification.component.dart
@@ -5,6 +5,6 @@ import 'package:angular2/core.dart';
     templateUrl: 'validation-notification.component.html',
     styleUrls: const ['validation-notification.component.css'])
 class ValidationNotificationComponent {
-  @Input()
-  String message;
+  @Input() String forId;
+  @Input() String message;
 }
