b2e1d350592060df445bd3f3a85641c6194a936e
Ne4istb <anechytailov@gmail.com>
2016-06-19 12:44:03 +0300

    [SP-78] Fixed validation notification component

--- lib/shared/components/validation-notification/validation-notification.component.dart
+++ lib/shared/components/validation-notification/validation-notification.component.dart
@@ -1,10 +1,25 @@
 import 'package:angular2/core.dart';
+import 'dart:html';
 
 @Component(
     selector: 'sp-validation-notification',
     templateUrl: 'validation-notification.component.html',
     styleUrls: const ['validation-notification.component.css'])
-class ValidationNotificationComponent {
+class ValidationNotificationComponent implements OnInit {
   @Input() String forId;
   @Input() String message;
+
+  ElementRef _element;
+
+  ValidationNotificationComponent(this._element);
+
+  @override
+  ngOnInit() {
+    Element e = _element.nativeElement;
+
+    var notificationStyle = e.querySelector('.validation-notification').style;
+
+    e.parent.onMouseOver.listen((_) =>  notificationStyle.transform = 'scale(1)');
+    e.parent.onMouseOut.listen((_) =>  notificationStyle.transform = 'scale(0)');
+  }
 }
