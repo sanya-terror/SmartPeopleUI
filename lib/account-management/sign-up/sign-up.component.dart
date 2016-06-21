7b60c4fd56533ebf9a222f096e5cdde4c2d96a84
Ne4istb <anechytailov@gmail.com>
2016-06-21 20:12:48 +0300

    [SP-84] Applied new design on sign-up form

--- lib/account-management/sign-up/sign-up.component.dart
+++ lib/account-management/sign-up/sign-up.component.dart
@@ -1,7 +1,7 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/common.dart';
 import 'package:angular2/router.dart';
-import 'package:angular2_rbi/directives.dart' show MaterialButton, MaterialTextfield, MaterialRadio;
+import 'package:angular2_rbi/directives.dart' show MaterialRadio;
 
 import 'package:SmartPeopleUI/shared/index.dart';
 import 'index.dart';
@@ -11,37 +11,48 @@ import 'index.dart';
     directives: const [
       ROUTER_DIRECTIVES,
       ErrorTooltipComponent,
-      MaterialButton,
-      MaterialTextfield,
+      InputComponent,
+      CheckboxComponent,
+      LinkComponent,
+      CardComponent,
+      ButtonComponent,
       MaterialRadio
     ],
     styleUrls: const ['sign-up.component.css'],
     encapsulation: ViewEncapsulation.Emulated,
     templateUrl: 'sign-up.component.html')
 class SignUpComponent extends FormComponent {
+
   ControlGroup form;
+  Control nameControl;
+  Control surnameControl;
+  Control emailControl;
+  Control passwordControl;
+  Control passwordRepeatControl;
+  Control genderControl;
 
   SignUpComponent() {
+    this.nameControl = new Control('', Validators.compose([NameValidator.validate, Validators.required]));
+    this.surnameControl = new Control('', Validators.compose([NameValidator.validate, Validators.required]));
+    this.emailControl = new Control('', Validators.compose([EmailValidator.validate, Validators.required]));
+    this.passwordControl = new Control('',
+       Validators.compose([
+         PasswordValidator.validate,
+         Validators.required,
+         Validators.minLength(6),
+         Validators.maxLength(18)
+       ]));
+    this.passwordRepeatControl = new Control('',
+          Validators.compose([PasswordValidator.validate, Validators.required]));
+    this.genderControl = new Control('male');
+
     this.form = new ControlGroup({
-      'name': new Control('',
-          Validators.compose([NameValidator.validate, Validators.required])),
-      'surname': new Control('',
-          Validators.compose([NameValidator.validate, Validators.required])),
-      'email': new Control('',
-          Validators.compose([EmailValidator.validate, Validators.required])),
-      'password': new Control(
-          '',
-          Validators.compose([
-            PasswordValidator.validate,
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(18)
-          ])),
-      'passwordRepeat': new Control(
-          '',
-          Validators
-              .compose([PasswordValidator.validate, Validators.required])),
-      'gender': new Control('', Validators.required)
+      'name': this.nameControl,
+      'surname': this.surnameControl,
+      'email': this.emailControl,
+      'password': this.passwordControl,
+      'passwordRepeat': this.passwordRepeatControl,
+      'gender': this.genderControl
     });
   }
 }
