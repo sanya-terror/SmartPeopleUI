5ce0cdfcc6808d6c42125af3ab52e4175c64d5c8
Ne4istb <anechytailov@gmail.com>
2016-06-23 00:20:59 +0300

    [SP-68] Fixed some tests

--- test/account-management/restore-access/restore-access.component.dart
+++ test/account-management/restore-access/restore-access.component.dart
@@ -103,11 +103,11 @@ class RestoreAccessComponentTests {
 
           expect(component.isCodeSent, true);
         });
+      });
 
-        test('Should set default on destroy component', () {
-          component.ngOnDestroy();
-          expect(verify(mockStore.dispatch(argThat(predicate((action) => action.type == CLEAR_RESTORE_ACCESS)))).callCount, 1);
-        });
+      test('Should set default on destroy component', () {
+        component.ngOnDestroy();
+        expect(verify(mockStore.dispatch(argThat(predicate((action) => action.type == CLEAR_RESTORE_ACCESS)))).callCount, 1);
       });
 
       test('Should set default', () {
