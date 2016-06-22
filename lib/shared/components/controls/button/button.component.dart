70a3ac9da695ecb8ccd3cb1331e840613cc928d5
Ne4istb <anechytailov@gmail.com>
2016-06-23 00:10:28 +0300

    [SP-84] Fixed button emitter

--- lib/shared/components/controls/button/button.component.dart
+++ lib/shared/components/controls/button/button.component.dart
@@ -1,4 +1,4 @@
-import 'package:angular2/angular2.dart' show Component, Input, Output, EventEmitter;
+import 'package:angular2/angular2.dart' show Component, Input;
 import 'package:angular2/router.dart' show Router;
 import 'package:angular2_rbi/directives.dart' show MaterialButton;
 
@@ -11,17 +11,13 @@ class ButtonComponent {
    @Input() String label;
    @Input() List<String> route;
    @Input() bool accent = false;
-   @Output('click') EventEmitter handler = new EventEmitter();
 
    Router _router;
 
    ButtonComponent(this._router);
 
    onClick() {
-      handler.emit(true);
-
       if (route == null) return;
-
       _router.navigate(route);
    }
 }
