675b41c2135084e66802b95ffe1b699057a94304
Ne4istb <anechytailov@gmail.com>
2016-06-23 00:23:43 +0300

    [SP-81] Removed info component

--- lib/shared/components/index.dart
+++ lib/shared/components/index.dart
@@ -1,6 +1,5 @@
 export 'main/index.dart';
 export 'footer/index.dart';
-export 'info/index.dart';
 export 'form.component.dart';
 export 'error-tooltip/index.dart';
 export 'controls/index.dart';
--- lib/shared/components/info/info.component.dart
+++
@@ -1,8 +0,0 @@
-import 'package:angular2/core.dart';
-
-@Component(
-    selector: 'sp-info',
-    templateUrl: 'info.component.html',
-    encapsulation: ViewEncapsulation.None,
-    styleUrls: const ['info.component.css'])
-class InfoComponent {}
