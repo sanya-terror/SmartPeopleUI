15cc4cb6b5ae22c44b06e37af6f418e360109cbb
ne4istb <anechytailov@gmail.com>
2016-07-24 13:38:52 +0300

    [SP-39] Fix travis build, attempt 10

--- tool/travis.sh
+++ tool/travis.sh
@@ -10,30 +10,10 @@ echo Fetch Dart channel: $CHANNEL
 
 URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest
 DART_SDK_URL=$URL_PREFIX/sdk/dartsdk-linux-x64-release.zip
-if [[ "${BROWSERS,,}" =~ "dartium" ]]; then
-  DARTIUM_URL=$URL_PREFIX/dartium/dartium-linux-x64-release.zip
-fi
+DARTIUM_URL=$URL_PREFIX/dartium/dartium-linux-x64-release.zip
 
-parallel_get() {(
-  _download_and_unzip() {
-    # TODO(chirayu):  Hack to run npm in parallel.  Better to refactor and run
-    # both npm install and pub install in parallel.  Both must be run AFTER any
-    # rebasing onto g3v1x.  Also "pub install" can only happen after the Dart
-    # SDK is unzipped and the path has been updated.
-    if [[ "$1" == "npm" ]]; then
-      npm install
-      return
-    fi
-
-    ZIPFILE=${1/*\//}
-    curl -O -L $1 && unzip -q $ZIPFILE && rm $ZIPFILE
-  }
-  export -f _download_and_unzip
-
-  echo "$@" | xargs -d ' ' -n 1 -P 2 -I URL bash -c '_download_and_unzip URL'
-)}
-
-parallel_get npm $DART_SDK_URL $DARTIUM_URL
+npm install $DART_SDK_URL
+npm install $DARTIUM_URL
 
 echo Fetched new dart version $(<dart-sdk/version)
 
