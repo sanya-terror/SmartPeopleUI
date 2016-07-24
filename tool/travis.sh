1699192406cc1d5add627d0dd0562e64bbbb5ca9
ne4istb <anechytailov@gmail.com>
2016-07-24 13:26:52 +0300

    [SP-39] Fix travis build, attempt 9

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
 # Fast fail the script on failures.
-set -e
+set -e -o pipefail
 
-DART_DIST=dartsdk-linux-x64-release.zip
-DARTIUM_DIST=dartium-linux-x64-release.zip
+AVAILABLE_DART_VERSION=$(curl "https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest/VERSION" | python -c \
+    'import sys, json; print(json.loads(sys.stdin.read())["version"])')
 
-echo Fetching dart sdk and Dartium
-curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
-curl http://storage.googleapis.com/dart-archive/channels/stable/raw/latest/dartium/$DARTIUM_DIST > $DARTIUM_DIST
+echo Fetch Dart channel: $CHANNEL
 
-unzip $DART_DIST > /dev/null
-unzip $DARTIUM_DIST > /dev/null
-rm $DART_DIST
-rm $DARTIUM_DIST
+URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest
+DART_SDK_URL=$URL_PREFIX/sdk/dartsdk-linux-x64-release.zip
+if [[ "${BROWSERS,,}" =~ "dartium" ]]; then
+  DARTIUM_URL=$URL_PREFIX/dartium/dartium-linux-x64-release.zip
+fi
 
-mv dartium-* dartium;
+parallel_get() {(
+  _download_and_unzip() {
+    # TODO(chirayu):  Hack to run npm in parallel.  Better to refactor and run
+    # both npm install and pub install in parallel.  Both must be run AFTER any
+    # rebasing onto g3v1x.  Also "pub install" can only happen after the Dart
+    # SDK is unzipped and the path has been updated.
+    if [[ "$1" == "npm" ]]; then
+      npm install
+      return
+    fi
 
-export DART_SDK="$PWD/dart-sdk"
-export PATH="$DART_SDK/bin:$PATH"
-export DARTIUM_BIN="$PWD/dartium/chrome"
+    ZIPFILE=${1/*\//}
+    curl -O -L $1 && unzip -q $ZIPFILE && rm $ZIPFILE
+  }
+  export -f _download_and_unzip
 
-echo Pub install
-pub install
+  echo "$@" | xargs -d ' ' -n 1 -P 2 -I URL bash -c '_download_and_unzip URL'
+)}
+
+parallel_get npm $DART_SDK_URL $DARTIUM_URL
+
+echo Fetched new dart version $(<dart-sdk/version)
+
+if [[ -n $DARTIUM_URL ]]; then
+  mv dartium-* chromium
+fi
 
 sh -e /etc/init.d/xvfb start
 
@@ -45,3 +62,5 @@ if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
     --exclude-test-files \
     test/all_test.dart
 fi
+
+
