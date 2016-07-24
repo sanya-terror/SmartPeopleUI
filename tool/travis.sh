21d2e0d501be73aec11b0f588be9ebec6ff31f6c
ne4istb <anechytailov@gmail.com>
2016-07-24 13:13:39 +0300

    [SP-39] Fix travis build, attempt 7

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,6 +3,30 @@
 # Fast fail the script on failures.
 set -e
 
+DART_DIST=dartsdk-linux-x64-release.zip
+DARTIUM_DIST=dartium-linux-x64-release.zip
+
+echo Fetching dart sdk and Dartium
+curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
+curl http://storage.googleapis.com/dart-archive/channels/stable/raw/latest/dartium/$DARTIUM_DIST > $DARTIUM_DIST
+
+unzip $DART_DIST > /dev/null
+unzip $DARTIUM_DIST > /dev/null
+rm $DART_DIST
+rm $DARTIUM_DIST
+
+mv dartium-* dartium;
+
+export DART_SDK="$PWD/dart-sdk"
+export PATH="$DART_SDK/bin:$PATH"
+export DARTIUM_BIN="$PWD/dartium/chrome"
+
+echo Pub install
+pub install
+
+sh -e /etc/init.d/xvfb start
+
+
 # Verify that the libraries are error free.
 dartanalyzer --fatal-warnings \
   lib/*.dart \
@@ -11,7 +35,7 @@ dartanalyzer --fatal-warnings \
 # Run browser tests
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
-pub run test -p firefox
+pub run test -p dartium
 
 # Verify the coverage of the tests.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
