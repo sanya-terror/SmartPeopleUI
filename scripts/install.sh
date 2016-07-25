298bee68f6d889ef0378fc5ee44dbcae79867ed6
Ne4istb <anechytailov@gmail.com>
2016-07-25 23:32:36 +0300

    [SP-33] Split travis script to smaller parts

--- scripts/install.sh
+++ scripts/install.sh
@@ -32,23 +32,3 @@ echo Pub install
 pub install
 
 sh -e /etc/init.d/xvfb start
-
-# Verify that the libraries are error free.
-dartanalyzer --fatal-warnings \
-  lib/*.dart \
-  test/*.dart
-
-# Run browser tests
-pub run test -p dartium
-
-# Verify the coverage of the tests.
-#if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "1.15.0" ]; then
-#  pub global activate dart_coveralls
-#  pub global run dart_coveralls report \
-#    --token $COVERALLS_TOKEN \
-#    --retry 2 \
-#    --exclude-test-files \
-#    test/all_test.dart
-#fi
-
-
