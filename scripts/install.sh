75d62da52aea9a2ee89fae93f73643439bed80b9
Ne4istb <anechytailov@gmail.com>
2016-07-27 21:18:39 +0300

    [SP-33] Added build step, small clean up

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,22 +3,15 @@
 # Fast fail the script on failures.
 set -e
 
-DART_DIST=dartsdk-linux-x64-release.zip
 DARTIUM_DIST=dartium-linux-x64-release.zip
 
-echo Fetching dart sdk and Dartium
-curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
+echo Fetching Dartium
 curl http://storage.googleapis.com/dart-archive/channels/stable/raw/latest/dartium/$DARTIUM_DIST > $DARTIUM_DIST
 
-unzip $DART_DIST > /dev/null
 unzip $DARTIUM_DIST > /dev/null
-rm $DART_DIST
 rm $DARTIUM_DIST
 
 mv dartium-* chromium
-
-export DART_SDK="$PWD/dart-sdk"
-export PATH="$DART_SDK/bin:$PATH"
 
 export DARTIUM_PATH="$PWD/chromium"
 export DARTIUM_BIN="$DARTIUM_PATH/chrome"
