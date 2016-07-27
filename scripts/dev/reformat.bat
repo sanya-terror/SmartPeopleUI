9d0ae744a719fe497aff53f7b21aaa10fb28ff6c
Ne4istb <anechytailov@gmail.com>
2016-07-27 21:42:10 +0300

    [SP-33] Added auto formatter scripts

--- scripts/dev/reformat.bat
+++ scripts/dev/reformat.bat
@@ -1,3 +1,3 @@
 call pub global activate dart_style
-call dartfmt lib -w -l 120
-call dartfmt test -w - l 120
+call dartfmt ../../lib -w -l 120
+call dartfmt ../../test -w -l 120
--- scripts/dev/reformat.sh
+++ scripts/dev/reformat.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pub global activate dart_style
-dartfmt lib -w -l 120
-dartfmt test -w - l 120
+dartfmt ../../lib -w -l 120
+dartfmt ../../test -w -l 120
